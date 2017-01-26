7e4210752dd6294f9b002b074710b8204445ebf3
Gugi Gustaman <hyourinseta@live.com>
2017-01-27 00:26:27 +0700

    tambah method cariLuasLingkaran

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -4,6 +4,10 @@ class HitungLuas2 {
 		return panjang * lebar;
 	}
 
+	public static float cariLuasLingkaran(int jarijari) {
+		return (22/7) * jarijari * jarijari;
+	}
+
 	public static void main (String[] args) {
 
 	}
